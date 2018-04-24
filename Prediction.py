4a2304a3508b5ab9bafb49312531c0d009a7c97a
Vaibhav Khamgaonkar <vaibhav.08k@gmail.com>
2018-04-24 21:10:52 +0530

    file or prediction single image

---
+++ Prediction.py
@@ -0,0 +1,25 @@
+## To do the prediction :
+#loading the saved model
+m = load_model('/Users/Digit.h5') # path of stored model
+
+#definging the functiuon to perform prediction
+
+def predictDigit(imgNum):
+    img = cv2.imread('/Users/' + str(imgNum), 0) # location of image 
+    print(img.shape)
+    #img = cv2.resize(img, (28,28))
+    img = img.reshape(1,28,28,1)
+    predict = m.predict_classes(img)
+    print(dM.predict(img))
+    print ('Digit is {}'.format(predict))
+    
+    
+    
+predictDigit('img1.jpg')
+
+
+
+Output is 
+(28, 28)
+[[1. 0. 0. 0. 0. 0. 0. 0. 0. 0.]]
+Digit is [0]
